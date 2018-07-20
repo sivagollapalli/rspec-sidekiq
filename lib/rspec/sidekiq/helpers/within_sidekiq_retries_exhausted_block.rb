696db06eb78c38a4ac15780b934891a80b6e5188
yankovskiid <yankovskiidv@gmail.com>
2018-07-20 13:08:27 +0300

    Add exception param to within_sidekiq_retries_exhausted_block helper

--- lib/rspec/sidekiq/helpers/within_sidekiq_retries_exhausted_block.rb
+++ lib/rspec/sidekiq/helpers/within_sidekiq_retries_exhausted_block.rb
@@ -1,9 +1,9 @@
 module Sidekiq
   module Worker
     module ClassMethods
-      def within_sidekiq_retries_exhausted_block(user_msg = {}, &block)
+      def within_sidekiq_retries_exhausted_block(user_msg = {}, exception = default_retries_exhausted_exception, &block)
         block.call
-        sidekiq_retries_exhausted_block.call default_retries_exhausted_args.merge(user_msg)
+        sidekiq_retries_exhausted_block.call(default_retries_exhausted_message.merge(user_msg), exception)
       end
 
       def default_retries_exhausted_args
@@ -13,6 +13,10 @@ module Sidekiq
           'args' => [],
           'error_message' => 'An error occured'
         }
+      end
+
+      def default_retries_exhausted_exception
+        StandardError.new('An error occured')
       end
     end
   end
